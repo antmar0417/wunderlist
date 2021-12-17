3278e6e2e3e8c2f13942d9301aefde7cd18a8c9e
antmar0417 <tonyboy@live.se>
2021-12-17 11:26:56 +0100

    Create quotes.php

---
+++ quotes.php
@@ -0,0 +1,24 @@
+<?
+
+declare(strict_types=1);
+
+$errors = [];
+$quotes = [
+    ['name' => '', 'quote' => ''],
+];
+
+if (isset($_POST['name'], $_POST['quote'])) {
+
+    $name = trim($_POST['name']);
+    $quote = trim($_POST['qu$quote']);
+
+    if ($name === '') {
+        $errors[] = 'You need to fill a name';
+    }
+    if ($quote === '') {
+        $errors[] = 'Please add content into the field';
+    }
+    if (count($errors) === 0) {
+        $quotes[] = ['name' => $name, 'quote' => $quote];
+    }
+}
