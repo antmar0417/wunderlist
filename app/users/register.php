089fd0b3e58caef3cc1044c7cfb2e783aa5f3518
antmar0417 <tonyboy@live.se>
2021-12-21 23:03:10 +0100

    added content into register.php

--- app/users/register.php
+++ app/users/register.php
@@ -5,5 +5,18 @@ declare(strict_types=1);
 require __DIR__ . '/../autoload.php';
 
 // In this file we register a new user.
+if (isset($_POST['email'], $_POST['password'], $_POST['name'])) {
+    $name = trim($_POST['name']);
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+
+    $statement = $database->prepare('INSERT INTO users (name, email, password) VALUES (:name, :email, :password)');
+
+    $statement->bindParam(':name', $name, PDO::PARAM_STR);
+    $statement->bindParam(':email', $email, PDO::PARAM_STR);
+    $statement->bindParam(':password', $password, PDO::PARAM_STR);
+
+    $statement->execute();
+}
 
 redirect('/');
