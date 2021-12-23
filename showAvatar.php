430a1e27e90e63fb9c9b92d81306d2bb7838e6b4
antmar0417 <tonyboy@live.se>
2021-12-23 20:26:09 +0100

    added a function to check if the avatar exist

--- showAvatar.php
+++ showAvatar.php
@@ -1,8 +1,3 @@
 <?php if (isset($_SESSION['user'])) : ?>
-    <p>Welcome, <?php echo $_SESSION['user']['name']; ?>!</p>
-    <?php if (file_exists(__DIR__ . '/uploads' . '/' . $_SESSION['user']['image'])) : ?>
-        <img src="/uploads/<?= $_SESSION['user']['image'] ?>" alt="Avatar Photo" height="100px">
-    <?php else : ?>
-        <img src="/uploads/unknown-avatar.jpeg" alt="Avatar Photo" height="100px">
-    <?php endif; ?>
+    <img src="<?= checkIfImageExist(); ?>" alt="Avatar Photo" height="100px">
 <?php endif; ?>
