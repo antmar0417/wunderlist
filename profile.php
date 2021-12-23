b602ca1817ac4add702b4a668288bdd6eec72318
antmar0417 <tonyboy@live.se>
2021-12-23 15:18:46 +0100

    added an user id verification

--- profile.php
+++ profile.php
@@ -2,18 +2,19 @@
 <?php require __DIR__ . '/views/header.php'; ?>
 <?php
 
-if (isset($_FILES['avatar'])) {
+if (isset($_FILES['avatar'], $_POST['id'])) {
     $avatar = $_FILES['avatar'];
     // Adding the user id and date into the image name
     $avatarName = $_SESSION['user']['id'] . '-' . date('ymd') . '-' . $avatar['name'];
     $destination = __DIR__ . '/uploads' . '/' . $avatarName;
     move_uploaded_file($avatar['tmp_name'], $destination);
     // Updating the image src into database
-    $id = $_SESSION['user']['id'];
+    $id = trim($_POST['id']);
     $query = "UPDATE users
     SET image = :image
-    WHERE id = $id";
+    WHERE id = :id";
     $statement = $database->prepare($query);
+    $statement->bindParam(':id', $id, PDO::PARAM_INT);
     $statement->bindParam(':image', $avatarName, PDO::PARAM_STR);
     $statement->execute();
 }
@@ -30,26 +31,28 @@ if (isset($_FILES['avatar'])) {
         <?php endif; ?>
         <form action="profile.php" method="post" enctype="multipart/form-data">
             <div>
+                <input type="hidden" name="id" value="<?= $_SESSION['user']['id'] ?>">
+
                 <label for="avatar">Please choose an image</label>
                 <input type="file" name="avatar" id="avatar" accept=".png, .jpg, jpeg" required>
             </div>
 
             <button type="submit">Upload</button>
+        </form>
 
-            <div>Change your email address</div>
-            <form action="" method="post">
-                <label for="email_address">email address:</label>
-                <input type="email" name="email_address" id="email_address">
-                <button type="submit">Change email address</button>
-            </form>
+        <div>Change your email address</div>
+        <form action="" method="post">
+            <label for="email_address">email address:</label>
+            <input type="email" name="email_address" id="email_address">
+            <button type="submit">Change email address</button>
+        </form>
 
-            <div>Change your password</div>
-            <form action="" method="post">
-                <label for="password">type a new password:</label>
-                <input name="password" id="password" type="password">
+        <div>Change your password</div>
+        <form action="" method="post">
+            <label for="password">type a new password:</label>
+            <input name="password" id="password" type="password">
 
-                <button type="submit">Change password</button>
-            </form>
+            <button type="submit">Change password</button>
         </form>
     <?php else : ?>
         <div class="row mt-5 justify-content-md-center">
