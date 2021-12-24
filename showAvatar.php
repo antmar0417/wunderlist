ab016a21b7b4ed0428e7d2eaf99f178615ddc36c
antmar0417 <tonyboy@live.se>
2021-12-24 16:47:11 +0100

    deleted showAvatar.php and updateAvatar.php

--- showAvatar.php
+++
@@ -1,3 +0,0 @@
-<?php if (isset($_SESSION['user'])) : ?>
-    <img src="<?= checkIfAvatarExist(); ?>" alt="Avatar Photo" height="100px">
-<?php endif; ?>
--- updateAvatar.php
+++
@@ -1,8 +0,0 @@
-<?php if (isset($_SESSION['user'])) : ?>
-    <p><?php echo $_SESSION['user']['name']; ?>, here is your new avatar</p>
-    <?php if (file_exists(__DIR__ . '/uploads' . '/' . $avatarName)) : ?>
-        <img src="/uploads/<?= $avatarName ?>" alt="Avatar Photo" height="100px">
-    <?php else : ?>
-        <img src="/uploads/unknown-avatar.jpeg" alt="Avatar Photo" height="100px">
-    <?php endif; ?>
-<?php endif; ?>
