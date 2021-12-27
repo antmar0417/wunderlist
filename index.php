c61e4a449c21f247c2d0dd973f8dd5366b3b112d
antmar0417 <tonyboy@live.se>
2021-12-27 22:06:17 +0100

    added a div for styling

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php require __DIR__ . '/app/autoload.php'; ?>
 <?php require __DIR__ . '/views/header.php'; ?>
 
-<article>
-    <h1><?php echo $config['title']; ?></h1>
-    <p>This is the home page.</p>
-    <?php if (isset($_SESSION['user'])) : ?>
-        <img src="<?= checkIfAvatarExist(); ?>" alt="Avatar Photo" height="100px">
-    <?php endif; ?>
-</article>
+<div class="row mt-4 justify-content-md-center">
+    <div class="col-md-3 ml-center">
+        <h1><?php echo $config['title']; ?></h1>
+        <p>This is the home page.</p>
+        <?php if (isset($_SESSION['user'])) : ?>
+            <img src="<?= checkIfAvatarExist(); ?>" alt="Avatar Photo" height="100px">
+        <?php endif; ?>
+    </div>
+</div>
 
 <?php require __DIR__ . '/views/footer.php'; ?>
