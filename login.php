2b5b72a2b1c261192ffcfe3181695c017f48daae
antmar0417 <tonyboy@live.se>
2021-12-27 22:05:25 +0100

    added divs for styling

--- login.php
+++ login.php
@@ -1,24 +1,27 @@
 <?php require __DIR__ . '/app/autoload.php'; ?>
 <?php require __DIR__ . '/views/header.php'; ?>
 
-<article>
-    <h1>Login</h1>
+<div class="row mt-5 justify-content-md-center">
+    <div class="col-md-6 ml-center">
+        <h1>Login</h1>
 
-    <form action="app/users/login.php" method="post">
-        <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
-            <input class="form-control" type="email" name="email" id="email" placeholder="your@email.com" required>
-            <small class="form-text">Please provide the your email address.</small>
-        </div>
+        <form action="app/users/login.php" method="post">
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input class="form-control" type="email" name="email" id="email" placeholder="your@email.com" required>
+                <small class="form-text">Please provide the your email address.</small>
+            </div>
 
-        <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
-            <input class="form-control" type="password" name="password" id="password" required>
-            <small class="form-text">Please provide the your password (passphrase).</small>
-        </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input class="form-control" type="password" name="password" id="password" required>
+                <small class="form-text">Please provide the your password (passphrase).</small>
+            </div>
 
-        <button type="submit" class="btn btn-primary">Login</button>
-    </form>
-</article>
+            <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+    </div>
+</div>
+
 
 <?php require __DIR__ . '/views/footer.php'; ?>
