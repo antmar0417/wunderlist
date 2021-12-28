d0cd83dfbcdb2cfff64315cb40886345b3145d17
antmar0417 <tonyboy@live.se>
2021-12-28 21:17:54 +0100

    created /password/upload.php file

---
+++ app/users/password/upload.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../../autoload.php';
+
+
+// Check if both password and id exists in the POST request.
+if (isset($_POST['password'], $_POST['id'])) {
+    $password = trim($_POST['password']);
+    $id = trim($_POST['id']);
+
+    $query = "UPDATE users
+    SET password = :password
+    WHERE id = :id";
+
+    $statement = $database->prepare($query);
+    $statement->bindParam(':id', $id, PDO::PARAM_INT);
+    $statement->bindParam(':password', $password, PDO::PARAM_STR);
+    $statement->execute();
+}
+
+redirect('/profile.php');
