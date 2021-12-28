2534a2eea593bf72e97f981b8a0d7bdd93795da4
antmar0417 <tonyboy@live.se>
2021-12-28 20:44:21 +0100

    updating email variable in avatar/upload.php

--- app/users/avatar/upload.php
+++ app/users/avatar/upload.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 require __DIR__ . '/../../autoload.php';
 
+$avatarName = '';
+
 if (isset($_FILES['avatar'], $_POST['id'])) {
     $avatar = $_FILES['avatar'];
+
     // Adding the user id and date into the image name
     $avatarName = $_SESSION['user']['id'] . '-' . date('ymd') . '-' . $avatar['name'];
     $destination = __DIR__ . '/../../../uploads' . '/' . $avatarName;
     move_uploaded_file($avatar['tmp_name'], $destination);
+
     // Updating the image src into database
     $id = trim($_POST['id']);
     $query = "UPDATE users
@@ -19,10 +23,13 @@ if (isset($_FILES['avatar'], $_POST['id'])) {
     $statement->bindParam(':id', $id, PDO::PARAM_INT);
     $statement->bindParam(':image', $avatarName, PDO::PARAM_STR);
     $statement->execute();
+
     // Updating session variables without re-login
+    $email = $_SESSION['user']['email'];
     if (($query)) {
         $user = $statement->fetch(PDO::FETCH_ASSOC);
         $_SESSION['user'] = $user;
+        $_SESSION['user']['email'] = $email;
         $_SESSION['user']['image'] = $avatarName;
         $_SESSION['user']['id'] = $_POST['id'];
     }
