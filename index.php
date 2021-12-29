6d0199a9bcfdecbe739a38bc0778aa6b37b0a4b6
antmar0417 <tonyboy@live.se>
2021-12-29 20:13:37 +0100

    added an image

--- index.php
+++ index.php
@@ -3,14 +3,24 @@
 
 <div class="row mt-4 justify-content-md-center">
     <div class="col-md-3 ml-center">
-        <h1><?php echo $config['title']; ?></h1>
-        <?php if (isLoggedIn()) : ?>
-            <p>Welcome, <?php echo $_SESSION['user']['name']; ?>!</p>
-            <img src="<?= checkIfAvatarExist(); ?>" alt="Avatar Photo" height="100px">
-        <?php else : ?>
-            <p>Login or Sign in to create tasks</p>
-        <?php endif; ?>
+        <h1 class="text-center"><?php echo $config['title']; ?></h1>
     </div>
 </div>
+
+<?php if (isLoggedIn()) : ?>
+    <div class="col-md-3 ml-center">
+        <p>Welcome, <?php echo $_SESSION['user']['name']; ?>!</p>
+        <img src="<?= checkIfAvatarExist(); ?>" alt="avatar photo" height="100px">
+    </div>
+<?php else : ?>
+    <div class="row mt-4 justify-content-md-center">
+        <div class="col-md-3 ml-center">
+            <p class="text-center">Login or Sign in to create tasks</p>
+        </div>
+    </div>
+    <div class="row mt-0 justify-content-md-center">
+        <img src="./images/to-do-list.jpeg" alt="to-do list photo">
+    </div>
+<?php endif; ?>
 
 <?php require __DIR__ . '/views/footer.php'; ?>
