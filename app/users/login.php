0034a5f0b102bc3b1e07372b821bee97573aa985
antmar0417 <tonyboy@live.se>
2021-12-29 01:33:28 +0100

    removed the comments

--- app/users/login.php
+++ app/users/login.php
@@ -3,14 +3,9 @@
 declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
-// $errors = [];
-
 
 // Check if both email and password exists in the POST request.
 if (isset($_POST['email'], $_POST['password'])) {
-    // $email = trim($_POST['email']);
-    // test
-    // $password = trim($_POST['password']);
     $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
 
     // Prepare, bind email parameter and execute the database query.
@@ -20,20 +15,6 @@ if (isset($_POST['email'], $_POST['password'])) {
 
     // Fetch the user as an associative array.
     $user = $statement->fetch(PDO::FETCH_ASSOC);
-
-    // If we couldn't find the user in the database, redirect back to the login
-    // page with our custom redirect function.
-    // if (!$user) {
-    //     redirect('/login.php');
-    // }
-    // Check if the email adress is valid
-    // if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    //     $errors[] = 'The email address is not a valid email address!';
-    // }
-
-    // if ($email !== $user['email'] || $password !== $user['password']) {
-    //     $errors[] = 'Whoops... The provided credentials does not match our records!';
-    // }
 
     // If we found the user in the database, compare the given password from the
     // request with the one in the database using the password_verify function.
@@ -54,7 +35,3 @@ if (isset($_POST['email'], $_POST['password'])) {
         redirect('/login.php');
     }
 }
-
-// We should put this redirect in the end of this file since we always want to
-// redirect the user back from this file. We don't know
-// redirect('/');
