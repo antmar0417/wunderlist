7f27ecc35f0a0f55530b49a4e2bd15ec5e6b534a
antmar0417 <tonyboy@live.se>
2022-01-04 18:50:08 +0100

    created lists.php

---
+++ lists.php
@@ -0,0 +1,15 @@
+<?php if (isset($_GET['list']))
+    $id = $_SESSION['user']['id'];
+$statement = $database->query("SELECT DISTINCT title
+    FROM lists
+    INNER JOIN users
+    ON users.id = lists.user_id
+    WHERE lists.user_id = $id
+    ORDER BY lists.title DESC");
+
+$lists = $statement->fetchAll(PDO::FETCH_ASSOC);
+?>
+<h2>Your lists</h2>
+<?php foreach ($lists as $list) : ?>
+    <p><?php echo $list['title']; ?></p>
+<?php endforeach; ?>
