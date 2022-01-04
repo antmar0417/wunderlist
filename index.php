99b7127384094e266fbcae44090a15c5a506cd7c
antmar0417 <tonyboy@live.se>
2022-01-04 18:51:08 +0100

    performed test to show users lists

--- index.php
+++ index.php
@@ -8,10 +8,30 @@
 </div>
 
 <?php if (isLoggedIn()) : ?>
-    <div class="col-md-3 ml-center">
-        <p>Welcome, <?php echo $_SESSION['user']['name']; ?>!</p>
+    <div class="row mt-4 justify-content-md-center">
+        <div class="col-md-3 ml-center">
+            <p class="text-center">Welcome, <?php echo $_SESSION['user']['name']; ?>!</p>
+        </div>
+    </div>
+    <div class="d-flex justify-content-center">
         <img src="<?= checkIfAvatarExist(); ?>" alt="avatar photo" height="100px">
     </div>
+    <div class="d-flex justify-content-center">
+        <details class="navbar-brand">
+            <summary>
+                Lists
+            </summary>
+            <ul class="list-group list-group-flush">
+                <a href="index.php?list=all" class="list-group-item list-group-item-action">All</a>
+                <a href="#" class="list-group-item list-group-item-action">Ongoing</a>
+                <a href="#" class="list-group-item list-group-item-action">Done</a>
+            </ul>
+        </details>
+    </div>
+    <?php if (isset($_GET['list'])) : ?>
+        <p class="text-center">Your list, <?php echo $_GET['list']; ?></p>
+        <?php require __DIR__ . '/lists.php'; ?>
+    <?php endif; ?>
 <?php else : ?>
     <div class="row mt-4 justify-content-md-center">
         <div class="col-md-3 ml-center">
