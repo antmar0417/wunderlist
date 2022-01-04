a765751f6a6a7bbffa383ac94f0ac8e6bb339294
antmar0417 <tonyboy@live.se>
2022-01-04 19:40:05 +0100

    Update lists.php

--- lists.php
+++ lists.php
@@ -1,15 +1,15 @@
 <?php
-
-if (isset($_GET['list']))
+if (isset($_GET['list'])) {
     $id = $_SESSION['user']['id'];
-$statement = $database->query("SELECT DISTINCT title
+    $statement = $database->query("SELECT DISTINCT title
     FROM lists
     INNER JOIN users
     ON users.id = lists.user_id
     WHERE lists.user_id = $id
     ORDER BY lists.title DESC");
 
-$lists = $statement->fetchAll(PDO::FETCH_ASSOC);
+    $lists = $statement->fetchAll(PDO::FETCH_ASSOC);
+}
 ?>
 
 <h2>Your lists</h2>
