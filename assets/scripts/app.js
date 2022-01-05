7a6493724fcbe2cde1b83b65ff5b3c2f560c2e11
antmar0417 <tonyboy@live.se>
2022-01-05 15:20:11 +0100

    Update app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,12 +1,1 @@
-const button = document.getElementById('button');
-const popUpList = document.querySelector('.pop-up-add-list');
-const closeAddList = document.querySelector('.close-add-list');
-const popUpAddList = document.querySelector('.pop-up-add-list');
-
-button.addEventListener('click', () => {
-  popUpList.style.display = 'flex';
-});
-
-closeAddList.addEventListener('click', () => {
-  popUpAddList.style.display = 'none';
-});
+console.log('hello');
