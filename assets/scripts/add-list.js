a18192329916429411cbcad7f3bc18a1a251ce9c
antmar0417 <tonyboy@live.se>
2022-01-05 15:29:56 +0100

    Update add-list.js

--- assets/scripts/add-list.js
+++ assets/scripts/add-list.js
@@ -2,11 +2,14 @@ const button = document.getElementById('open-button');
 const popUpList = document.querySelector('.pop-up-add-list');
 const closeAddList = document.querySelector('.close-add-list');
 const popUpAddList = document.querySelector('.pop-up-add-list');
+let count = 0;
 
 button.addEventListener('click', () => {
+  count++;
   popUpList.style.display = 'flex';
 });
 
 closeAddList.addEventListener('click', () => {
+  count++;
   popUpAddList.style.display = 'none';
 });
