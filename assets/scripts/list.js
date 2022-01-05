2e6f10caa77a684751415f9789cd625fc6c3318b
antmar0417 <tonyboy@live.se>
2022-01-05 16:09:45 +0100

    Update list.js

--- assets/scripts/list.js
+++ assets/scripts/list.js
@@ -1,7 +1,8 @@
 const openButton = document.getElementById('open-button');
 const popUpList = document.querySelector('.pop-up-add-list');
 const closeAddList = document.querySelector('.close-add-list');
+let count = 0;
 
 openButton.addEventListener('click', () => {
-  popUpList.style.display = 'flex';
+  count++;
 });
