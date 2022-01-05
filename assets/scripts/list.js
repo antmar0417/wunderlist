ff277d1c9175aab2661d83cbae6eedc80af7be7a
antmar0417 <tonyboy@live.se>
2022-01-05 15:45:18 +0100

    Create list.js

---
+++ assets/scripts/list.js
@@ -0,0 +1,3 @@
+const button = document.getElementById('open-button');
+const popUpList = document.querySelector('.pop-up-add-list');
+const closeAddList = document.querySelector('.close-add-list');
