534a4937009e92aa51200c5eb63ed01964e4a7b0
antmar0417 <tonyboy@live.se>
2022-01-05 15:26:32 +0100

    Update add-list.js

--- assets/scripts/add-list.js
+++ assets/scripts/add-list.js
@@ -1,4 +1,4 @@
-const button = document.getElementById('button');
+const button = document.getElementById('open-button');
 const popUpList = document.querySelector('.pop-up-add-list');
 const closeAddList = document.querySelector('.close-add-list');
 const popUpAddList = document.querySelector('.pop-up-add-list');
