24a6b760b9796527292544b76885b274e6f7e471
antmar0417 <tonyboy@live.se>
2022-01-05 16:34:01 +0100

    Delete list.js

--- assets/scripts/list.js
+++
@@ -1,8 +0,0 @@
-const openButton = document.getElementById('open-button');
-const popUpList = document.querySelector('.pop-up-add-list');
-const closeAddList = document.querySelector('.close-add-list');
-let count = 0;
-
-openButton.addEventListener('click', () => {
-  count++;
-});
