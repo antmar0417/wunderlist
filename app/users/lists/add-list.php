518fc6574d71d41edfffad550fd41cb2d52e7e41
antmar0417 <tonyboy@live.se>
2022-01-05 19:23:00 +0100

    Create add-list.php

---
+++ app/users/lists/add-list.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../../autoload.php';
+
+if (isset($_POST['list-title'], $_POST['id'])) {
+    $user_id = trim($_POST['id']);
+    $title = $_POST['list-title'];
+
+    $query = 'INSERT INTO lists (title, user_id) VALUES (:title, :user_id)';
+
+    $statement = $database->prepare($query);
+    $statement->bindParam(':title', $title, PDO::PARAM_STR);
+    $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $statement->execute();
+}
+?>
+
+<?php redirect('/index.php'); ?>
