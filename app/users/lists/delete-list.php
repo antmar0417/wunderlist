5028a0b15a8a4c540d2468e41624248cbb810ba8
antmar0417 <tonyboy@live.se>
2022-01-12 15:13:15 +0100

    Update delete-list.php

--- app/users/lists/delete-list.php
+++ app/users/lists/delete-list.php
@@ -6,7 +6,7 @@ require __DIR__ . '/../../autoload.php';
 
 if (isset($_GET['list-title'])) {
     $user_id = $_SESSION['user']['id'];
-    $title = $_GET['list-title'];
+    $title = trim($_GET['list-title']);
 
     $statement = $database->query("DELETE FROM lists
     WHERE user_id = $user_id AND title = '$title'");
