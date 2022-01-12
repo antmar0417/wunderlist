3b34179a3780fe51e847f0f571e44d28b24adc36
antmar0417 <tonyboy@live.se>
2022-01-12 15:25:49 +0100

    Update get-all-task-today.php

--- app/users/tasks/get-all-task-today.php
+++ app/users/tasks/get-all-task-today.php
@@ -8,7 +8,7 @@ if (isset($_GET['tasks-today'])) {
 
     $statement = $database->query("SELECT *
     FROM tasks
-    WHERE user_id = $id AND deadline_date = '$date' AND checked = 'No'");
+    WHERE user_id = $id AND deadline_date = '$date'");
 
     $tasks = $statement->fetchAll(PDO::FETCH_ASSOC);
 }
