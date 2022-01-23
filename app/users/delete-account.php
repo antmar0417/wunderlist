16eb087681bc924a63c6f64a2e089ab85d400fad
antmar0417 <tonyboy@live.se>
2022-01-23 11:31:13 +0100

    Removed blank lines

--- app/users/delete-account.php
+++ app/users/delete-account.php
@@ -8,15 +8,12 @@ if (isset($_POST['delete-account'], $_POST['id'])) {
     $userId = $_POST['id'];
     $userEmail = trim($_POST['delete-account']);
     if ($userEmail === $_SESSION['user']['email']) {
-
         $statement = $database->prepare('DELETE FROM users WHERE id = :id');
         $statement->bindParam(':id', $userId, PDO::PARAM_INT);
         $statement->execute();
-
         $statement = $database->prepare('DELETE FROM lists WHERE user_id = :user_id');
         $statement->bindParam(':user_id', $userId, PDO::PARAM_INT);
         $statement->execute();
-
         $statement = $database->prepare('DELETE FROM tasks WHERE user_id = :user_id');
         $statement->bindParam(':user_id', $userId, PDO::PARAM_INT);
         $statement->execute();
